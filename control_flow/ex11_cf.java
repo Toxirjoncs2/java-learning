5cbad39069e2ff8098cafdb305c00f6193f74363
Toxirjon <vaxobovtoxirjon@gmail.com>
2026-07-30 11:55:07 +0500

    Ex11

---
+++ control_flow/ex11_cf.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class ex11_cf     {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter a color: ");
+        String color=scanner.nextLine().toLowerCase();
+        switch (color){
+            case "g" -> System.out.println("Go!");
+            case "y" -> System.out.println("Get ready!");
+            case "r" -> System.out.println("Stop!");
+            default -> System.out.println("Invalid input");
+        }
+
+
+    }
+}
