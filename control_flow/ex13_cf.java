f81717aec2b7bb42f36e858911420846868a658f
Toxirjon <vaxobovtoxirjon@gmail.com>
2026-07-30 17:48:17 +0500

    Ex13

--- control_flow/ex13_cf.java
+++ control_flow/ex13_cf.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class ex13_cf {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter your score: ");
+        int grade=scanner.nextInt();
+        if (grade<0 || grade>100){
+            System.out.println("Invalid score");
+        }else{
+            grade/=10;
+            switch (grade) {
+                case 10 -> System.out.println("Your grade is A");
+                case 9 -> System.out.println("Your grade is A");
+                case 8 -> System.out.println("Your grade is B");
+                case 7 -> System.out.println("Your grade is C");
+                case 6 -> System.out.println("Your grade is D");
+                default -> System.out.println("Your grade is F");
+            }
+        }
+
+
+
+
+
+        scanner.close();
+
+    }
 }
