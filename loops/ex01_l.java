fbedbb6615147998481f49334f80172d455d2f08
Toxirjon <vaxobovtoxirjon@gmail.com>
2026-08-05 10:13:26 +0500

    Ex1

---
+++ loops/ex01_l.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ex01_l  {
+    public static void  main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        for(int i=1;i<=10;i++){
+            System.out.print(i+" ");
+        }
+
+
+        scanner.close();
+    }
+}
